878a66687d8de4e89e77ace0c1ebd5449572a002
Joshua Luke <214234169@mycput.ac.za>
2021-05-11 17:45:18 +0200

    another commit

--- src/main/java/za/ac/cput/EmployeeSet.java
+++ src/main/java/za/ac/cput/EmployeeSet.java
@@ -1,5 +1,42 @@
 package za.ac.cput;
 
+import java.util.*;
+
 public class EmployeeSet {
+
+    private String id, firstName, lastName;
+    private int age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
 }
